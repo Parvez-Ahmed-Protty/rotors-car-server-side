c928ebfff989bd6eafb4fb6ca6412bf6bd7e469a
Parvez-Ahmed-Protty <parvezahmedp22wd@gmail.com>
2021-11-22 21:02:54 +0600

    add repo

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csgkr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// console.log(uri);
 
 const run = async () => {
   try {
